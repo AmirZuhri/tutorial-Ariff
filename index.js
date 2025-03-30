8d0a854707133ec085e6b2af7d60ca02bada45cd
AmirZuhri <amirzuhriahmad@gmail.com>
2025-03-30 17:23:27 +0800

    For Loop Example Corrections

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const name = 'Bob';
 const now = new Date();
 console.log(`${name} - ${now.toLocaleString()}`);
+
+const names = ['Harris', 'Bala', 'Charlie'];  
+for (const eachName of names) {             //for loop example. forgot to open with () after for
+        console.log(eachName);
+    }
